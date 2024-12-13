cf61ec972d07128ca2f5be3505088143a93f922a
Oleg T <olegtimer@yandex.ru>
2024-12-13 10:41:24 +0500

    add help json

--- main.py
+++ main.py
@@ -2,9 +2,9 @@ from src.hh_api import HeadHunterAPI
 
 
 def main():
-    employers = [
-        # "80", # "Альфа-Банк"
-        # "1740", # "Яндекс"
+    employer_ids = [
+        "80", # "Альфа-Банк"
+        "1740", # "Яндекс"
         # "4181",# "ВТБ"
         # "1373",# "Аэрофлот"
         # "39305",# "Газпром нефть"
@@ -13,7 +13,7 @@ def main():
         # "4233",# "X5 Group"
         # "4219",# "Tele2"
         # "78638",# "Тинькофф"
-        "Альфа-Банк",
+        # "Альфа-Банк",
         # "Яндекс",
         # "ВТБ",
         # "Аэрофлот",
@@ -25,17 +25,9 @@ def main():
         # "Тинькофф",
     ]
     hh_api = HeadHunterAPI()
-    vacancies_list = []
-    i_count = 1
-    for employer in employers:
-        # vacancies_list.extend(hh_api.get_vacancies(employer))
-        print(i_count, end="-")
-        i_count += 1
-    print()
-    print(vacancies_list)
-    # for employer in employers_list:
-    #     if employer["open_vacancies"] > 0:
-    #         print(employer["name"]+" "+str(employer["open_vacancies"]))
+    data = hh_api.get_vacancies(employer_ids)
+
+    print(data)
 
 
 if __name__ == '__main__':
--- src/hh_api.py
+++ src/hh_api.py
@@ -8,30 +8,25 @@ class HeadHunterAPI:
 
     def __init__(self):
         """Конструктор класса HeadHunter для поиска вакансий."""
-        self.__url = "https://api.hh.ru/vacancies"
+        # self.__url = "https://api.hh.ru/vacancies"
         self.__headers = {"User-Agent": "HH-User-Agent"}
         self.__params = {"text": "", "page": 0, "per_page": 100}
         self.vacancies = []
 
-
-    def get_vacancies(self, keyword: str) -> list[dict[str, Any]]:
-        """Метод загрузки вакансий с НН"""
-        self.__params["text"] = keyword
-        while self.__params.get("page") != 20:
-            response = requests.get(self.__url, headers=self.__headers, params=self.__params)
+    def get_vacancies(self, employer_ids: list[str]) -> list[dict[str, Any]]:
+        """Метод загрузки вакансий по списку работодателей."""
+        i_count = 1
+        for employer_id in employer_ids:
+            # https: // api.hh.ru / vacancies?employer_id = 80
+            # self.__params["text"] = employer
+            # while self.__params.get("page") != 20:
+            response = requests.get(f"https://api.hh.ru/vacancies?employer_id={employer_id}", headers=self.__headers, params=self.__params)
             # if response.status_code != 200:
             #     continue
             vacancies = response.json()["items"]
             self.vacancies.extend(vacancies)
             self.__params["page"] += 1
+            print(i_count, end="-")
+            i_count += 1
+        print()
         return self.vacancies
-
-
-if __name__ == "__main__":
-    hh_api = HeadHunterAPI()
-    # employers_list = hh_api.get_employers()
-    vacancies_list = hh_api.get_vacancies("Альфа-Банк")
-    print(vacancies_list)
-    # for employer in employers_list:
-    #     if employer["open_vacancies"] > 0:
-    #         print(employer["name"]+" "+str(employer["open_vacancies"]))
